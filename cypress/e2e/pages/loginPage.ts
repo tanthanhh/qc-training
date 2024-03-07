1736eb189dde1975df317acaeebc77a4b3cd6f65
Tan Thanh Pham <tanthanh.pham@calcuso.com>
2024-03-07 22:48:08 +0700

    test: page render test

---
+++ cypress/e2e/pages/loginPage.ts
@@ -0,0 +1,22 @@
+const LoginPage = {
+    userName: '#userName',
+    password: '#password',
+    loginButton: '#login',
+    newUserButton: '#newUser',
+    output: '#output',
+    userNameLabel: '#userName-label',
+    errorMessage: '#name',
+
+    userNameXpath: '//*[@id="userName"]',
+    passwordXpath: '//*[@id="password"]',
+    loginButtonXpath: '//*[@id="login"]',
+    newUserButtonXpath: '//*[@id="newUser"]',
+    userNameLabelXpath: '//*[@id="userName-label"]',
+
+    userClass: '.form-control #userName',
+    passwordClass: '.form-control #password',
+    loginButtonClass: '.btn #login',
+    newUserButtonClass: '.btn #newUser',
+}
+
+export default LoginPage;
---
+++ cypress/e2e/pages/testRenderPage.ts
@@ -0,0 +1,11 @@
+let pageSelectors = {
+    'Buttons': 'div.col-12.mt-4.col-md-6 > div:nth-child(2) > h1',
+    'Links': '#linkWrapper > h1',
+    'Broken Links - Images': 'div.col-12.mt-4.col-md-6 > div:nth-child(2) > h1',
+    'Upload and Download': 'div.col-12.mt-4.col-md-6 > div:nth-child(2) > h1',
+    'Web Tables': 'div.col-12.mt-4.col-md-6 > div:nth-child(2) > h1',
+    'Dynamic Properties': 'div.col-12.mt-4.col-md-6 > div:nth-child(2) > h1',
+    'webTables': 'div.col-12.mt-4.col-md-6 > h1',
+};
+
+export default pageSelectors;
